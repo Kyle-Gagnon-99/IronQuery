d61ff319a16bd6f6d3906b968c8e6f15ca5c9e79
Kyle Gagnon <kmgagnon99@gmail.com>
2024-06-18 15:36:02 +0000

    del(all): Removing Rust from project

--- docker/postgres/init-postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copy any SQL init files to /docker-entrypoint-initdb.d
-# Adjust log file permissions
-chmod -R 644 /var/lib/postgresql/data/pg_logs/*.log
-
-# Call original entry point script
-docker-entrypoint.sh postgres
