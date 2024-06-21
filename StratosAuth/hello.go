7f947d25c06638d2c337a3d5c96bca1242349b7f
Kyle Gagnon <kmgagnon99@gmail.com>
2024-06-21 00:45:23 +0000

    chore: Add StratosDb, StratosAuth, and devcontainer configurations

--- StratosAuth/hello.go
+++ StratosAuth/hello.go
@@ -6,12 +6,40 @@ import (
 	"os"
 )
 
+type PlaygroundTest struct {
+	StratosAuthPort string
+	TestThing       uint8
+}
+
+type IPlaygroundTest interface {
+	DoSomething()
+}
+
+func (p *PlaygroundTest) Init() {
+	p.StratosAuthPort = "8080"
+	p.TestThing = 1
+}
+
+func (p *PlaygroundTest) DoSomething() {
+	log.Println("Doing something")
+}
+
+func PassInterface(p IPlaygroundTest) {
+	p.DoSomething()
+}
+
 func main() {
 	log.Println("Starting the application...")
 
 	// Get the PORT from the environment variables
 	port := os.Getenv("STRATOS_AUTH_PORT")
 	log.Println("STRATOS_AUTH_PORT: ", port)
+
+	// Create a new PlaygroundTest object
+	playgroundTest := PlaygroundTest{}
+	playgroundTest.Init()
+
+	PassInterface(&playgroundTest)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Received a request")
